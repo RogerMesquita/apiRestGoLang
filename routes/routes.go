db2bb5689181c1ddfdb4e2b6a27ce64d4478c278
rogermesquita <rogermesquitaa@hotmail.com>
2022-06-28 23:32:02 -0300

    CORS

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"apiRest/controllers"
 	"apiRest/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -18,5 +19,5 @@ func HandleRequest() {
 	r.HandleFunc("/api/personalities/new", controllers.New).Methods("POST")
 	r.HandleFunc("/api/personalities/{id}/delete", controllers.Delete).Methods("DELETE")
 	r.HandleFunc("/api/personalities/{id}/edit", controllers.Edit).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
